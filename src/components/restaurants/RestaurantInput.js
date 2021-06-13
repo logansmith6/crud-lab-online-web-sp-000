818fbd7a06bc84f96022cd9e1c32d34f9dffb67d
Logan Smith <logsmith06@gmail.com>
2021-06-13 22:55:44 +0000

    Automatically backed up by Learn

--- src/components/restaurants/RestaurantInput.js
+++ src/components/restaurants/RestaurantInput.js
@@ -25,7 +25,7 @@ class RestaurantInput extends Component {
       <form onSubmit={(event) => this.handleOnSubmit(event)}>
         <input
           type="text"
-          value={this.state.bandName}
+          value={this.state.text}
           onChange={(event) => this.handleOnChange(event)} />
         <input type="submit" />
       </form>
