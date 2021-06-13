71d445f112d23d86f12ba98e4f62d484bf45f626
Logan Smith <logsmith06@gmail.com>
2021-06-13 23:56:25 +0000

    Automatically backed up by Learn

--- src/containers/ReviewsContainer.js
+++ src/containers/ReviewsContainer.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import ReviewInput from '../components/reviews/ReviewInput';
 import Reviews from '../components/reviews/Reviews';
+import {connect} from 'react-redux'
 
 class ReviewsContainer extends Component {
 
@@ -8,11 +9,17 @@ class ReviewsContainer extends Component {
   render() {
     return (
       <div>
-        <ReviewInput />
-        <Reviews />
+        <ReviewInput restaurantId={this.props.restaurant.id} {...this.props.actions} />
+        <Reviews reviews={this.props.reviews.filter(rev => rev.restaurantId === this.props.restaurant.id)} actions={this.props.actions} />
       </div>
     )
   }
 }
 
+const mapStateToProps = dispatch => ({
+  actions: {
+    addReview: review=> dispatch({type: 'ADD_REVIEW', review})
+    deleteReview: id=> dispatch({type: 'DELETE_REVIEW', id: id})
+  }
+})
 export default ReviewsContainer;
