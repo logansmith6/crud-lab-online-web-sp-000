748a7ef14286370ac1c30f1b2813de28ef2d465a
Logan Smith <logsmith06@gmail.com>
2021-06-13 23:16:34 +0000

    Automatically backed up by Learn

--- src/reducers/manageRestaurant.js
+++ src/reducers/manageRestaurant.js
@@ -5,7 +5,7 @@ export default function manageRestaurants(state = {
 }, action) {
   switch (action.type) {
     case 'ADD_RESTAURANT':
-      const restaurant = { name: action.name}
+      const restaurant = {id: Math.random(), name: action.name}
       return {...state, restaurants: [...state.restaurants, restaurant]}
 
   default:
