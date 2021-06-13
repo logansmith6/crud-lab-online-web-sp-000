b72b83ac73439362cce34a62ba64018d2382fdb2
Logan Smith <logsmith06@gmail.com>
2021-06-13 23:06:36 +0000

    Automatically backed up by Learn

--- src/components/restaurants/Restaurants.js
+++ src/components/restaurants/Restaurants.js
@@ -2,9 +2,9 @@ import React, { Component } from 'react';
 import Restaurant from './Restaurant'
 
 class Restaurants extends Component {
-  const Restaurants = props => {
+
     const restaurants = props.restaurants.map(restaurant => <Restaurant key={restaurant.id} />)
-  }
+  
   render() {
     return(
       <ul>
