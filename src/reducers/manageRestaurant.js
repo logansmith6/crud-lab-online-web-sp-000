1d0a44e0bf8a9e046df9ee63c1f002a3d1c753d1
Logan Smith <logsmith06@gmail.com>
2021-06-13 23:36:51 +0000

    Automatically backed up by Learn

--- src/reducers/manageRestaurant.js
+++ src/reducers/manageRestaurant.js
@@ -5,6 +5,8 @@ function manageRestaurants(state=[], action) {
   switch (action.type) {
     case 'ADD_RESTAURANT':
       return [...state, {text: action.text, id: cuid()}]
+    case 'DELETE_RESTAURANT'
+      return state.filter(rest=> rest.id !== action.id)
 
   default:
       return state;
