c546caeed04205a761af252f8d3268c3d13c130f
Logan Smith <logsmith06@gmail.com>
2021-06-13 23:24:23 +0000

    Automatically backed up by Learn

--- src/components/restaurants/Restaurants.js
+++ src/components/restaurants/Restaurants.js
@@ -6,7 +6,7 @@ class Restaurants extends Component {
   render() {
     return(
       <ul>
-        Restaurants Component
+        {this.props.restaurants.map(restaurant => <Restaurant key={restaurant.id} restaurant={restaurant})}
       </ul>
     );
   }
