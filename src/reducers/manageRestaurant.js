21b14f8cd13c962e08f9e05a3c9c9cf03b96d1f8
Logan Smith <logsmith06@gmail.com>
2021-06-13 22:58:08 +0000

    Automatically backed up by Learn

--- src/reducers/manageRestaurant.js
+++ src/reducers/manageRestaurant.js
@@ -1,5 +1,11 @@
 import cuid from 'cuid';
 
-export default function manageRestaurants(state, action) {
+export default function manageRestaurants(state = {
+  restaurants: []
+}, action) {
+  switch (action.type) {
+    case 'ADD_RESTAURANT':
+      const restaurant = { name: action.name}
+  }
 
 }
