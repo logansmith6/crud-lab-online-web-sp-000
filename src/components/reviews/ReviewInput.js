2b8889cab409d40e4d475704ac1668555bbc0931
Logan Smith <logsmith06@gmail.com>
2021-06-13 23:50:14 +0000

    Automatically backed up by Learn

--- src/components/reviews/ReviewInput.js
+++ src/components/reviews/ReviewInput.js
@@ -5,7 +5,13 @@ class ReviewInput extends Component {
   render() {
     return (
       <div>
-        Review Input
+      <form onSubmit={(event) => this.handleOnSubmit(event)}>
+        <input
+          type="text"
+          value={this.state.text}
+          onChange={(event) => this.handleOnChange(event)} />
+        <input type="submit" />
+      </form>
       </div>
     );
   }
