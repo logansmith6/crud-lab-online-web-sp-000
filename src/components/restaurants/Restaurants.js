d42e9968a485b63355259e8b3bb392c7617b39e9
Logan Smith <logsmith06@gmail.com>
2021-06-13 23:04:34 +0000

    Automatically backed up by Learn

--- src/components/restaurants/Restaurants.js
+++ src/components/restaurants/Restaurants.js
@@ -2,10 +2,11 @@ import React, { Component } from 'react';
 import Restaurant from './Restaurant'
 
 class Restaurants extends Component {
+
   render() {
     return(
       <ul>
-        Restaurants Component
+       {restaurants}
       </ul>
     );
   }
